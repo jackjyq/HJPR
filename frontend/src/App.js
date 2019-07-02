f97ce7bedfd6b7c595ff294742c0cc388f1ed7fa
Jack Jiang <jackjyq@outlook.com>
2019-07-02 22:28:42 +1000

    add design.js & copyright footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React,  { Component} from 'react';
 import HomePage from "./HomePage"
 import './App.css';
+import Design from "./Design"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        {/* <HomePage /> */}
+        <Design />
+        
       </div>
+
     );
   }
 }
