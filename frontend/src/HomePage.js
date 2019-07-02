15df4fe6f1922c36086b0619b296630f518777e5
Jack Jiang <jackjyq@outlook.com>
2019-07-02 18:33:34 +1000

    add UserPreference

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,39 +1,50 @@
-import React, { Component} from 'react';
+import React from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
 import "./HomePage.css";
 import logo from './logo.svg';
 import UserPreference from "./UserPreference"
+import ShowProjectDescription from "./ShowProjectDescription"
 
-
-class HomePage extends Component {
+class HomePage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {hasStarted: true};
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
-        alert("Hi Jack and Rishabh")
+        // if (this.state.hasStarted) {
+        //     this.setState({hasStarted: false})
+        // } else {
+        //     this.setState({hasStarted: true})
+        // }
+        this.setState({hasStarted: true})
     }
+
     render() {
+        const hasStarted = this.state.hasStarted
         return (
             <div>
-            <Container>
-                <Row>
-                <Col sm="1" md = "3" className = "show-border">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </Col>
-                <Col sm="1" md ="9" className = "show-border">
-                    <Button color = "primary" onClick = {this.handleClick}> Get Started</Button>
-                </Col>
-                </Row>
-                <Row>
-                    <Col sm = "12">
-                    <Button color = "primary" onClick = {this.handleClick}> Get Started</Button>
-                    </Col>
-                </Row>
+                <Container>
+                    <Row>
+                        <Col md = "3" className = "border-control">
+                            <img src={logo} className="App-logo" alt="logo" />
+                        </Col>
+                        <Col md ="9" className = "border-control">
+                            <ShowProjectDescription></ShowProjectDescription>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm = "12" className = "border-control">
+                            <Button color = "primary" onClick = {this.handleClick}> Get Started</Button>
+                        </Col>
+                    </Row>
             </Container>
-            <UserPreference/>
+                {hasStarted > 0 &&
+                    <UserPreference></UserPreference>
+                }
             </div>
+
         )
     }
 }
--- frontend/src/UserPreference.js
+++ frontend/src/UserPreference.js
@@ -1,12 +1,77 @@
-import React, { Component} from 'react';
+import React from 'react';
 import "./UserPreference.css";
-import { Container, Row, Col, Button } from 'reactstrap';
+import {
+    Container,
+    Row,
+    Col,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupButtonDropdown,
+    InputGroupDropdown,
+    Input,
+    Button,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Label,
+    Form, 
+    FormGroup,
+    FormText
+   } from 'reactstrap';
 
-class UserPreference extends Component {
+class UserPreference extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return(
             <Container>
-                <Row>Tell us a little bit about yourself</Row>
+                <Row>
+                    Add Skills you want to learn
+                </Row>
+                <Row>
+                    <Col md = '4' className = "border-control">
+                        <InputGroup>
+                        <Input placeholder="Python" />
+                        <InputGroupAddon addonType="append"><Button>Add</Button></InputGroupAddon>
+                        </InputGroup>
+                    </Col>
+                    <Col md = '8' className = "border-control">
+                    </Col>
+                </Row>
+
+                <Row>
+                    Add course codes which you have already taken
+                </Row>
+                <Row>
+                    <Col md = '4' className = "border-control">
+                        <InputGroup>
+                        <Input placeholder="COMP9021" />
+                        <InputGroupAddon addonType="append"><Button>Add</Button></InputGroupAddon>
+                        </InputGroup>
+                    </Col>
+                    <Col md = '8' className = "border-control">
+                    </Col>
+                </Row>
+
+                <Row>
+                <Col>
+                <FormGroup>
+                    <Label for="exampleSelect">Select</Label>
+                    <Input type="select" name="select" id="exampleSelect">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Input>
+                    </FormGroup>
+                </Col>
+   
+                </Row>
+
             </Container>
         )
     }
