1567ad8efbb3185b99b7fdd6b5251cdd8d37b8b7
Himanshu Vaswani <39785853+HimanshuVaswani@users.noreply.github.com>
2019-07-23 23:23:52 +1000

    Suggest Recommended Courses

---
+++ backend/rakegensim/unsw_json/runner.py
@@ -0,0 +1,74 @@
+import gensim
+from nltk.tokenize import word_tokenize
+import csv
+import json
+
+def suggest(skills, courses, questions):
+    if(len(skills)==0):
+        value="Error"
+        return 1,value
+    userInfo=' '.join(skills)
+    flag=0
+    if(len(questions)!=0):
+        flag=1
+        userInfoMore=questions
+    if(flag==1):
+        userInfoTotal=userInfo+userInfoMore
+    else:
+        userInfoTotal=userInfo
+    model=gensim.similarities.Similarity.load("ModelSims")
+    tf_idf=gensim.similarities.Similarity.load("tf_idfModel")
+    dictCollection=gensim.similarities.Similarity.load("dictCollectionModel")
+    query=[l.lower() for l in word_tokenize(userInfoTotal)]
+    query_doc_bow = dictCollection.doc2bow(query)
+    query_doc_tf_idf = tf_idf[query_doc_bow]
+    listSimilarity=[]
+    j=0
+    for item in model[query_doc_tf_idf]:
+        listSimilarity.append((j, item))
+        j+=1
+    listSimilarity=sorted(listSimilarity, key=lambda x: x[1], reverse=True)
+
+    courseInfo={}
+    dictCourseInfo={}
+    with open('CourseDataCleaned-2.csv', 'r') as readFile:
+        k=0
+        while (1):
+            line = readFile.readline()
+            if not line:
+                break
+            i = line.find(",")
+            courseCode = line[:i]
+            if(courseCode!="CourseCode"):
+                courseDescription = str(line[i+1:])
+                courseDescription=courseDescription.strip()
+                courseInfo[courseCode] = courseDescription
+                dictCourseInfo[k]=[courseCode, courseDescription]
+            k+=1
+
+
+
+    sum1=0
+    len1=0
+    flg=0
+    for courseValue in listSimilarity[:8]:
+        if(courseValue[1]!=0.0):
+            sum1+=courseValue[1]
+            len1+=1
+        else:
+            flg=1
+            break
+    if(len1==0):
+        value="Error"
+        return 1,value
+    finalListToJson={}
+    listc=[]
+    for courseValue in listSimilarity[:8]:
+        if(courseValue[1]!=0.0):
+            tempc={}
+            tempc["label"]=dictCourseInfo[courseValue[0]][0]
+            tempc["value"]=courseValue[1]/sum1
+            tempc["desc"]=dictCourseInfo[courseValue[0]][1]
+            listc.append(tempc)
+    finalListToJson["courses"]=listc
+    return 0, finalListToJson
