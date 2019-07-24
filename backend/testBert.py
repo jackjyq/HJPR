74d7942fac4f23132e65de3ccd993961a7cb2f76
Jack Jiang <jackjyq@outlook.com>
2019-07-24 19:33:02 +1000

    add test file

--- backend/testBert.py
+++ backend/testBert.py
@@ -1,9 +1,15 @@
 from bert.runner import suggest as bert_suggest
 
 data_dict = {"skills": [ "analysis", "python", "problem solving"], "courses": [ "TSMA2004","TSMA4100", "AUT2201"],"questions": "I like python"}
+# parameter = {
+#     "skills": [ "analysis", "python", "problem solving"],
+#     "courses": [ "COMP1000", "COMP1511"],
+#     "questions": "Molecular and cell biology"
+# }
+
 parameter = {
-    "skills": [ "analysis", "python", "problem solving"],
-    "courses": [ "COMP1000", "COMP1511"],
-    "questions": "Molecular and cell biology"
+    "skills": [ "python"],
+    "courses": [ "COMP1000"],
+    "questions": "I lke programming"
 }
 print (bert_suggest(parameter))
